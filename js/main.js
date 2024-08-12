1d7f23ae384e4947306fcdeda8a973c446e09c00
DESKTOP-RLIRLHO\user <lhi7247@naver.com>
2024-08-12 16:24:30 +0900

    js

--- js/main.js
+++ js/main.js
@@ -1,27 +1,25 @@
 window.onload = function () {
-<<<<<<< HEAD
- // topbt기능
- const topBtn = document.querySelector("#gotop");
- topBtn.addEventListener("click", function (e) {
-   e.preventDefault();
-   if (window.scrollY !== 0) {
-     // 현재 스크롤 위치가 0이 아니면 top으로 스크롤
-     window.scrollTo({
-       top: 0,
-       behavior: "smooth",
-     });
-   }
- });
-=======
->>>>>>> 7c0ed2748ab34a3d1fd01fac6d378e07829a6c01
+  // topbt기능
+  const topBtn = document.querySelector("#gotop");
+  topBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    if (window.scrollY !== 0) {
+      // 현재 스크롤 위치가 0이 아니면 top으로 스크롤
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+  });
+
   // 상단의 마이페이지리스트 기능
   const mypageBt = document.querySelector(".mypage-bt");
   const mypageList = document.querySelector(".mypage-list");
   mypageBt.addEventListener("click", function () {
     mypageList.classList.toggle("showMypage");
   });
-
-  // 프로모션 스와이퍼 기능
+  // swiper ================
+  // 프로모션 스와퍼기능
   const swPromotion = new Swiper(".sw-promotion", {
     slidesPerView: 1,
     spaceBetween: 24,
@@ -38,7 +36,7 @@ window.onload = function () {
       prevEl: ".promotion .sw-prev",
     },
     pagination: {
-      el: ".sw-promotion-pg",
+      el: ".sw-promotion-pg ",
       clickable: true,
     },
     loop: true,
@@ -48,28 +46,40 @@ window.onload = function () {
     },
   });
 
-  // 쇼핑 스와이퍼 기능
+  // 쇼핑의 스와이퍼 기능적용
   const swShopping = new Swiper(".sw-shopping", {
-    slidesPerView: 1.1,
-    grid: {
-      rows: 1,
-      fill: "row",
-    },
     navigation: {
       nextEl: ".shopping .sw-next",
       prevEl: ".shopping .sw-prev",
     },
     loop: true,
-    pagination: {
-      el: ".sw-shopping-pg",
-      clickable: true,
+    slidesPerView: 1.1,
+    grid: {
+      rows: 1,
+      fill: "row",
     },
     spaceBetween: 10,
     breakpoints: {
-      1024: {
+      480: {
         spaceBetween: 32,
         slidesPerView: 2,
-        slidesPerGroup: 2,
+        grid: {
+          rows: 1,
+        },
+        slidesPerGroup: 3,
+      },
+      680: {
+        spaceBetween: 32,
+        slidesPerView: 3,
+        grid: {
+          rows: 1,
+        },
+        slidesPerGroup: 3,
+      },
+      1024: {
+        spaceBetween: 32,
+        slidesPerView: 3,
+        slidesPerGroup: 3,
         grid: {
           rows: 1,
         },
@@ -84,59 +94,61 @@ window.onload = function () {
       },
     },
   });
-  // 투어 스와이퍼 기능
+  // 투어의 스와이퍼 기능적용
   const swTour = new Swiper(".sw-tour", {
+    // 모바일버전
     slidesPerView: 1.2,
     spaceBetween: 10,
+    slidesPerGroup: 2,
     grid: {
-      rows: 4,
+      rows: 1,
       fill: "row",
-    },
-    loop: true,
-    autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
     },
     navigation: {
       nextEl: ".tour .sw-next",
       prevEl: ".tour .sw-prev",
     },
-    pagination: {
-      el: ".sw-tour-pg",
-      clickable: true,
-    },
+    loop: true,
     breakpoints: {
-      800: {
+      480: {
+        spaceBetween: 32,
         slidesPerView: 2,
-        spaceBetween: 26,
         slidesPerGroup: 2,
+        grid: {
+          rows: 2,
+        },
       },
       1024: {
+        spaceBetween: 32,
         slidesPerView: 3,
-        spaceBetween: 30,
-        slidesPerGroup: 3,
+        slidesPerGroup: 2,
+        grid: {
+          rows: 1,
+        },
       },
       1280: {
+        spaceBetween: 26,
         slidesPerView: 3,
-        spaceBetween: 40,
-        slidesPerGroup: 4,
+        slidesPerGroup: 3,
+        grid: {
+          rows: 1,
+        },
       },
     },
   });
-  // 티켓 스와이퍼 기능
+  // ticket스와퍼
   const swTicket = new Swiper(".sw-ticket", {
-    loop: true,
-    autoplay: {
-      delay: 3000,
-      disableOnInteraction: false,
-    },
+    slidesPerView: 1.2,
+    spaceBetween: 10,
     navigation: {
       nextEl: ".ticket .sw-next",
       prevEl: ".ticket .sw-prev",
     },
+
+    loop: true,
     breakpoints: {
       480: {
-        spaceBetween: 10,
+        spaceBetween: 32,
         slidesPerView: 2,
       },
       1024: {
@@ -149,103 +161,112 @@ window.onload = function () {
       },
     },
   });
-// 라이브 스와이퍼 기능
-const swLive = new Swiper(".sw-live", {
-  loop: true,
-<<<<<<< HEAD
-  slidesPerView: 1.2,
-  spaceBetween: 10,
-=======
->>>>>>> 7c0ed2748ab34a3d1fd01fac6d378e07829a6c01
-  autoplay: {
-    delay: 3000,
-    disableOnInteraction: false,
-  },
-  navigation: {
-    nextEl: ".live .sw-next",
-    prevEl: ".live .sw-prev",
-  },
-  breakpoints: {
-    480: {
-      spaceBetween: 10,
-      slidesPerView: 2,
+  // live스와퍼
+  const swLive = new Swiper(".sw-live", {
+    navigation: {
+      nextEl: ".live .sw-next",
+      prevEl: ".live .sw-prev",
     },
-    1024: {
-      spaceBetween: 32,
-      slidesPerView: 3,
-    },
-    1280: {
-      spaceBetween: 26,
-      slidesPerView: 4,
-    },
-  },
-});
-<<<<<<< HEAD
-// 도서 스와이퍼 기능
-const swBooks = new Swiper(".sw-books", {
-  loop: true, // 무한 루프
-  slidesPerView: 1.2, // 기본적으로 화면에 1.2개의 슬라이드가 보임
-  spaceBetween: 10, // 슬라이드 간의 간격
-  grid: {
-    rows: 4,
-    fill: "row",
-  },
-  autoplay: {
-    delay: 3000, // 3초마다 슬라이드 자동 전환
-    disableOnInteraction: false, // 슬라이드 상호작용 후에도 자동 전환 유지
-  },
 
-  navigation: {
-    nextEl: ".section-slide .sw-next", // 다음 버튼
-    prevEl: ".section-slide .sw-prev", // 이전 버튼
-  },
+    loop: true,
+    slidesPerView: 1.2,
+    spaceBetween: 10,
+    breakpoints: {
+      330: {
+        slidesPerView: 1.2,
+        spaceBetween: 10,
+      },
+      480: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
 
-  breakpoints: {
-    480: {
-      spaceBetween: 10, // 화면 너비가 480px 이상일 때 슬라이드 간 간격
-      slidesPerView: 2, // 2개의 슬라이드가 보임
+      675: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      1280: {
+        slidesPerView: 4,
+        spaceBetween: 27,
+      },
     },
-    768: {
-      spaceBetween: 20, // 화면 너비가 768px 이상일 때 슬라이드 간 간격
-      slidesPerView: 3, // 3개의 슬라이드가 보임
+  });
+  // books스와퍼
+  const bookx = new Swiper(".sw-books", {
+    navigation: {
+      nextEl: ".books .sw-next",
+      prevEl: ".books .sw-prev",
     },
-    1024: {
-      spaceBetween: 30, // 화면 너비가 1024px 이상일 때 슬라이드 간 간격
-      slidesPerView: 4, // 4개의 슬라이드가 보임
-    },
-  },
-});
-// 이벤트 스와이퍼 기능
-const swEvent = new Swiper(".sw-event", {
-  loop: true, // 무한 루프
-  slidesPerView: 1.2, // 기본적으로 화면에 1.2개의 슬라이드가 보임
-  spaceBetween: 10, // 슬라이드 간의 간격
-  autoplay: {
-    delay: 3000, // 3초마다 슬라이드 자동 전환
-    disableOnInteraction: false, // 슬라이드 상호작용 후에도 자동 전환 유지
-  },
 
-  navigation: {
-    nextEl: ".section-slide .sw-next", // 다음 버튼
-    prevEl: ".section-slide .sw-prev", // 이전 버튼
-  },
+    loop: true,
+    slidesPerView: 2,
+    spaceBetween: 19,
+    grid: {
+      rows: 5,
+      fill: "row",
+    },
+    breakpoints: {
+      480: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+        grid: {
+          rows: 1,
+        },
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      1024: {
+        slidesPerView: 4,
+        slidesPerGroup: 3,
+        spaceBetween: 30,
+        grid: {
+          rows: 1,
+        },
+      },
+      1280: {
+        slidesPerView: 5,
+        slidesPerGroup: 5,
+        spaceBetween: 27,
+        grid: {
+          rows: 1,
+        },
+      },
+    },
+  });
+  // event 스와퍼
+  const swEvent = new Swiper(".sw-events", {
+    navigation: {
+      nextEl: ".event .sw-next",
+      prevEl: ".event .sw-prev",
+    },
+    slidesPerView: 2,
+    spaceBetween: 10,
+    breakpoints: {
+      480: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+        grid: {
+          rows: 1,
+        },
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
 
-  breakpoints: {
-    480: {
-      spaceBetween: 10, // 화면 너비가 480px 이상일 때 슬라이드 간 간격
-      slidesPerView: 2, // 2개의 슬라이드가 보임
+      1280: {
+        slidesPerView: 4,
+      },
     },
-    1024: {
-      spaceBetween: 20, // 화면 너비가 768px 이상일 때 슬라이드 간 간격
-      slidesPerView: 3, // 3개의 슬라이드가 보임
-    },
-    1280: {
-      spaceBetween: 30, // 화면 너비가 1024px 이상일 때 슬라이드 간 간격
-      slidesPerView: 4, // 4개의 슬라이드가 보임
-    },
-  },
-
-});
-=======
->>>>>>> 7c0ed2748ab34a3d1fd01fac6d378e07829a6c01
+  });
 };
